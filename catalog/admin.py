31635620333a98fbdfb83c08b0a46d18a00a9639
Artem <artemhod2003@gmail.com>
2022-04-11 12:59:24 +0500

    Add image/logo show in admin.py for catalog app

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,34 +1,58 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Product, Category, Brand, Feature, Product_Feature
 
-class ProductInline(admin.TabularInline):
+class ProductFeatureInline(admin.TabularInline):
     model = Product_Feature
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'count', 'category', 'brand')
-    inlines = [
-        ProductInline
-    ]
-
 
 class BrandInline(admin.TabularInline):
     model = Brand.category.through
-    
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'count', 'image_show', 'category', 'brand')
+    inlines = [
+        ProductFeatureInline
+    ]
+
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return None
+
+    image_show.__name__ = 'Миниатюра'
+    
+    
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'info')
+    list_display = ('name', 'logo_show', 'info')
     inlines = [
         BrandInline
     ]
 
+    def logo_show(self, obj):
+        if obj.logo:
+            return mark_safe("<img src='{}' width='60' />".format(obj.logo.url))
+        return None
+    
+    logo_show.__name__ = 'Лого'
+
+
+@admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('name', 'info')
+    list_display = ('name', 'logo_show','info')
+
+    def logo_show(self, obj):
+        if obj.logo:
+            return mark_safe("<img src='{}' height='30' />".format(obj.logo.url))
+        return None
+
+    logo_show.__name__ = 'Лого'
     
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Brand)
+
+
 admin.site.register(Feature)
 admin.site.register(Product_Feature)
