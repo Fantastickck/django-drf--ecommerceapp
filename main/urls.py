199571e6363afb131169424815d13a3431f2e138
Artem <artemhod2003@gmail.com>
2022-04-18 14:38:09 +0500

    Add orders by user, refactor profile, add sidebar in profile

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from .views import EditProfile, GetAdvUser, home, user_register, user_login, user_logout
+from .views import GetOrdersByUser, home, user_register, user_login, user_logout, GetOrEditProfile
 
 urlpatterns = [
     path('', home, name='home'),
     path('register/', user_register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('edit/', EditProfile.as_view(), name='edit_profile'),
-    path('user/<slug:slug>/', GetAdvUser.as_view(), name='get_user'),
-
+    path('user/orders/<slug:slug>', GetOrdersByUser.as_view(), name='get_orders'),
+    path('user/profile/<slug:slug>/', GetOrEditProfile.as_view(), name='get_user')
 ]
--- main/views.py
+++ main/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.views import View
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, ListView
 from django.views.decorators.csrf import csrf_protect
 
 from main.forms import EditProfileForm, UserLoginForm, UserRegisterForm
 
 from .models import AdvUser, Profile
+
+from order.models import Order, OrderItem
 
 
 def home(request):
@@ -62,10 +64,20 @@ def user_logout(request):
     return redirect('login')
 
 
-class GetAdvUser(DetailView):
-    model = Profile
-    template_name = 'main/user.html'
+class GetOrdersByUser(ListView):
+    model = Order
+    template_name = 'main/orders_by_user.html'
+    context_object_name = 'orders'
 
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     orders = Order.objects.filter(user__profile__slug=self.kwargs['slug'])
+    #     context['items_by_orders'] = {
+
+    #     }
+
+    def get_queryset(self):
+        return Order.objects.filter(user__profile__slug=self.kwargs['slug'])
 
 # def edit_profile(request):
 #     user = request.user
@@ -80,17 +92,19 @@ class GetAdvUser(DetailView):
 #         form = EditProfileForm(instance=profile)
 #     return render(request, 'main/edit_profile.html', {'form': form})
 
-class EditProfile(View):
-    def get(self, request):
+
+
+class GetOrEditProfile(View):
+    def get(self, request, slug):
         user = request.user
         profile = Profile.objects.get(user=user)
         form = EditProfileForm(instance=profile)
         context = {
             'form': form
         }
-        return render(request, 'main/edit_profile.html', context)
+        return render(request, 'main/user.html', context)
 
-    def post(self, request):
+    def post(self, request, slug):
         user = request.user
         form = EditProfileForm(request.POST)
         if form.is_valid():
